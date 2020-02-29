e1df2379ec31ffa059240442dc04c4308e91411a
Jim Marshall <jimmarshall35@gmail.com>
2020-02-29 14:12:13 +0000

    refactored blocks to use inheritance

--- js/blocks.js
+++ js/blocks.js
@@ -1,4 +1,4 @@
-class Block{
+class BlockBase{
 	constructor(rect, spritecoords, img, gridcoords){
 		this.rect = rect;
 		this.spritecoords = spritecoords;
@@ -6,20 +6,18 @@ class Block{
 		this.gridcoords = gridcoords
 		this.silver = false;
 		this.gold = false;
-		this.health = 1;
 		this.drawlayer = 2;
 		drawlist[this.drawlayer].push(this);
 	}
-
-	draw(){
-		let swidth = this.img.width/4;
-		let sheight = this.img.height/2;
-		let sx = this.spritecoords.x * swidth;
-		let sy = this.spritecoords.y * sheight;
-		ctx.drawImage(this.img, sx, sy, swidth, sheight, this.rect.pos.x, this.rect.pos.y, this.rect.w, this.rect.h);
+	
+	decrementHealth(){
+		this.health--;
+		if(this.health <= 0){
+			this.delete();
+			levelspawner.initcollidableblocks();
+		}
 	}
 	delete(){
-		//console.log(drawlist.length);
 		for(let i = 0; i<drawlist[this.drawlayer].length; i++){
 			if(drawlist[this.drawlayer][i] == this){
 				drawlist[this.drawlayer].splice(i, 1);
@@ -35,25 +33,37 @@ class Block{
 				levelspawner.collidableblocks.splice(i, 1);
 			}
 		}
-		//console.log(drawlist.length);
 	}
+
 }
-class SpecialBlock{
+class Block extends BlockBase{
 	constructor(rect, spritecoords, img, gridcoords){
-		this.gridcoords = gridcoords
-		this.rect = rect;
-		this.spritecoords = spritecoords;
-		this.img = img;
+		super(rect, spritecoords, img, gridcoords);
+		this.health = 1;
+	}
+	
+	draw(){
+		let swidth = this.img.width/4;
+		let sheight = this.img.height/2;
+		let sx = this.spritecoords.x * swidth;
+		let sy = this.spritecoords.y * sheight;
+		ctx.drawImage(this.img, sx, sy, swidth, sheight, this.rect.pos.x, this.rect.pos.y, this.rect.w, this.rect.h);
+	}
+	
+}
+class SpecialBlock extends BlockBase{
+	constructor(rect, spritecoords, img, gridcoords){
+		super(rect, spritecoords, img, gridcoords);
+
 		this.flashtime = 500;
 		this.waittime = 5000;
 		this.animationtimer;
 		this.waittimer = d.getTime();
 		this.onlyonce = true;
-		this.silver = false;
-		this.gold = false;
+	
 		this.health = 2;
-		this.drawlayer = 2;
-		drawlist[this.drawlayer].push(this);
+		
+		
 		updatelist.push(this);
 	}
 
@@ -84,27 +94,15 @@ class SpecialBlock{
 		}
 	}
 	delete(){
-		//console.log(drawlist.length);
-		for(let i = 0; i<drawlist[this.drawlayer].length; i++){
-			if(drawlist[this.drawlayer][i] == this){
-				drawlist[this.drawlayer].splice(i, 1);
-			}
-		}
+		super.delete();
 		for(let i = 0; i<updatelist.length; i++){
 			if(updatelist[i] == this){
 				updatelist.splice(i, 1);
 			}
 		}
-		for(let i = 0; i<levelspawner.blocks.length; i++){
-			if(levelspawner.blocks[i] == this){
-				levelspawner.blocks.splice(i, 1);
-			}
-		}
-		for(let i = 0; i<levelspawner.collidableblocks.length; i++){
-			if(levelspawner.collidableblocks[i] == this){
-				levelspawner.collidableblocks.splice(i, 1);
-			}
-		}
-		//console.log(drawlist.length);
+	}
+	decrementHealth(){
+		super.decrementHealth();
+		//this.spritecoords.y = 2;
 	}
 }
